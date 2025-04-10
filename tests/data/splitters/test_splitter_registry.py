b36f936358a8354687ae00f1d980864f44e0aff7
Julien Raynal <59729220+JulienRaynal@users.noreply.github.com>
2025-04-10 16:53:54 +0200

    FEAT: tests/data/splitters/test/splitter_registry.py -> Added a test to check for uniqueness of registeries

--- tests/data/splitters/test_splitter_registry.py
+++ tests/data/splitters/test_splitter_registry.py
@@ -1,11 +1,13 @@
 """Test for the Splitter Registry class."""
 
+from typing import Any
+
 import pytest
 
-from src.stimulus.core.registry import BaseRegistry
-from src.stimulus.data.splitting import AbstractSplitter
+from src.stimulus.core.registry import AbstractRegistry, BaseRegistry
 from src.stimulus.data.splitters.registry import SplitterRegistry
-from tests.core import mock
+from src.stimulus.data.splitting import AbstractSplitter
+from tests.core import mock  # noqa: F401
 
 
 class SplitterClass(AbstractSplitter, metaclass=SplitterRegistry):
@@ -48,15 +50,37 @@ class SplitterClass2(AbstractSplitter):
 
 def test_num_splitter_in_registry() -> None:
     """Checks that SplitterClass, SplitterClass1, SplitterClass2."""
-    splitter_registry_classes: dict[str,
-                                    AbstractSplitter] = SplitterRegistry.all()
+    splitter_registry_classes: dict[str, AbstractSplitter] = SplitterRegistry.all()
     assert len(splitter_registry_classes) == 4
 
 
+# We intentionnaly use the wrong type
 def test_wrong_model_registering() -> None:
+    """Checks that the model raisses an error on wrong class registering."""
     with pytest.raises(TypeError, match=".* must be subclass of"):
 
-        @SplitterRegistry.register("false_splitter")
+        @SplitterRegistry.register("false_splitter")  # noqa: arg-type
         class FalseSplitter:
-            def __init__():
+            """A class that should raise an error because it is not inheriting AbstractSplitter."""
+
+            def __init__(self):
                 pass
+
+
+def test_uniqueness_registry() -> None:
+    """Checks that registering in the SplitterRegistry doesn't save in the BaseRegistry."""
+    splitter_registry_classes: dict[str, AbstractSplitter] = SplitterRegistry.all()
+    base_registry_classes: dict[str, Any] = BaseRegistry.all()
+    abstract_registry_classes: dict[str, Any] = AbstractRegistry.all()
+
+    # Generate set to get unique objects and do faster comparison
+    splitter_registry_set: set[AbstractSplitter] = set(
+        splitter_registry_classes.values(),
+    )
+    base_registry_set: set[Any] = set(base_registry_classes.values())
+    abstract_registry_set: set[Any] = set(abstract_registry_classes.values())
+
+    # Check that there's no intersection between them
+    assert len(splitter_registry_set.intersection(base_registry_set)) == 0
+    assert len(splitter_registry_set.intersection(abstract_registry_set)) == 0
+    assert len(base_registry_set.intersection(abstract_registry_set)) == 0
