65d88ae57926c82a1977f5b9ac84b5feaca1dce1
mathysgrapotte <mathysgrapotte@gmail.com>
2025-04-24 14:10:38 +0200

    fix(tensors): predict and compare tensors now take into account all metrics outputed by model.

--- src/stimulus/cli/predict.py
+++ src/stimulus/cli/predict.py
@@ -55,6 +55,54 @@ def load_data_config_from_path(data_path: str, data_config_path: str) -> torch.u
     )
 
 
+def update_statistics(statistics: dict, temp_statistics: dict) -> dict:
+    """Update the statistics with the new statistics.
+
+    Args:
+        statistics: The statistics to update.
+        temp_statistics: The new statistics to update with.
+
+    Returns:
+        The updated statistics.
+    """
+    for key, value in temp_statistics.items():
+        if isinstance(value, torch.Tensor):
+            if value.ndim == 0:
+                try:
+                    # Check if statistics[key] is zero-dimensional and reshape it if needed
+                    if statistics[key].ndim == 0:
+                        statistics[key] = torch.cat([statistics[key].reshape(1), value.unsqueeze(0)], dim=0)
+                    else:
+                        statistics[key] = torch.cat([statistics[key], value.unsqueeze(0)], dim=0)
+                except RuntimeError as e:
+                    raise RuntimeError(
+                        f"Error updating statistics: {e}, shape of incoming tensors is {value.shape} and in-place tensor is {statistics[key].shape}, values of those tensors are {value} and {statistics[key]}",
+                    ) from e
+            else:
+                statistics[key] = torch.cat([statistics[key], value], dim=0)
+        elif isinstance(value, (int, float, list)):
+            statistics[key] = statistics[key] + value
+        else:
+            raise TypeError(f"Invalid statistics type: {type(value)}")
+
+    return statistics
+
+
+def convert_dict_to_tensor(data: dict) -> dict:
+    """Convert a dictionary to a tensor.
+
+    Args:
+        data: The dictionary to convert.
+
+    Returns:
+        The converted dictionary.
+    """
+    for key, value in data.items():
+        if not isinstance(value, torch.Tensor):
+            data[key] = torch.tensor(value)
+    return data
+
+
 def predict(
     data_path: str,
     data_config_path: str,
@@ -78,13 +126,13 @@ def predict(
 
     # create empty tensor for predictions
     is_first_batch = True
-    for x, _y, _meta in loader:
+    for x, y, _meta in loader:
         if is_first_batch:
-            predictions = model(**x)
+            _loss, statistics = model.batch(x, y)
             is_first_batch = False
-        temp_predictions = model(**x)
-        predictions = torch.cat((predictions, temp_predictions), dim=0)
+        _loss, temp_statistics = model.batch(x, y)
+        statistics = update_statistics(statistics, temp_statistics)
 
-    to_return: dict = {"predictions": predictions}
+    to_return: dict = convert_dict_to_tensor(statistics)
     # Predict the data
     safetensors.torch.save_file(to_return, output)
