ade079a0403ffff1dcd1cb5a5d9f03d2c670cc6d
Julien Raynal <59729220+JulienRaynal@users.noreply.github.com>
2025-04-30 14:26:54 +0200

    FEAT: src/stimulus/data/encoders tests/data/encoding/test_encoder_registry.py -> Added encoders registry

--- tests/core/mock.py
+++ tests/core/mock.py
@@ -1,6 +1,8 @@
 """A mock class to show the registering on import in tests/core/test_registry.py."""
 
 from src.stimulus.core.registry import BaseRegistry
+from src.stimulus.data.encoders.registry import EncoderRegistry
+from src.stimulus.data.encoding.encoders import AbstractEncoder
 from src.stimulus.data.splitters.registry import SplitterRegistry
 from src.stimulus.data.splitting.splitters import AbstractSplitter
 
@@ -21,3 +23,12 @@ class Splitter3(AbstractSplitter):
     def echo(self) -> str:
         """Returns just class3."""
         return "splitter_3"
+
+
+@EncoderRegistry.register("EnCOdEr_ClAss3")
+class Encoder3(AbstractEncoder):
+    """A mock class."""
+
+    def echo(self) -> str:
+        """Returns just class3."""
+        return "encoder_3"
