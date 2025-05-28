72f139098b2be93229c20ecea0e1b0439f0d75bf
mathysgrapotte <mathysgrapotte@gmail.com>
2025-05-28 09:30:04 +0200

    split now properly uses dataset dict objects and saves to disk using pyarrow formatting

--- src/stimulus/cli/split_csv.py
+++ src/stimulus/cli/split_csv.py
@@ -48,34 +48,41 @@ def split_csv(data_csv: str, config_yaml: str, out_path: str, *, force: bool = F
     # create a DatasetProcessor object from the config and the csv
     splitter, split_columns = load_splitters_from_config_from_path(config_yaml)
     try:
-        dataset = datasets.load_dataset("parquet", data_files=data_csv)
+        dataset_dict = datasets.load_dataset("parquet", data_files=data_csv)
     except pa.ArrowInvalid:
         logger.info("Data is not in parquet format, trying csv")
-        dataset = datasets.load_dataset("csv", data_files=data_csv)
+        dataset_dict = datasets.load_dataset("csv", data_files=data_csv)
 
-    if "test" in dataset and not force:
+    if "test" in dataset_dict and not force:
         logger.info("Test split already exists and force was set to False. Skipping split.")
-        dataset.save_to_disk(out_path)
+        dataset_dict.save_to_disk(out_path)
         return
 
-    dataset_with_numpy_format = dataset.with_format("numpy")
+    if "test" in dataset_dict and force:
+        logger.info(
+            "Test split already exists and force was set to True. Merging current test split into train and recalculating splits.",
+        )
+        dataset_dict["train"] = datasets.concatenate_datasets([dataset_dict["train"], dataset_dict["test"]])
+        del dataset_dict["test"]
+
+    dataset_dict_with_numpy_format = dataset_dict.with_format("numpy")
     column_data_dict = {}
     for col_name in split_columns:
         try:
-            column_data_dict[col_name] = dataset_with_numpy_format[col_name]
+            column_data_dict[col_name] = dataset_dict_with_numpy_format["train"][col_name]
         except KeyError as err:
             raise ValueError(
-                f"Column '{col_name}' not found in dataset with columns {dataset_with_numpy_format.column_names}",
+                f"Column '{col_name}' not found in dataset with columns {dataset_dict_with_numpy_format['train'].column_names}",
             ) from err
 
     if not column_data_dict:
         raise ValueError(
-            f"No data columns were extracted for splitting. Input specified columns are {split_columns}, dataset has columns {dataset_with_numpy_format.column_names}",
+            f"No data columns were extracted for splitting. Input specified columns are {split_columns}, dataset has columns {dataset_dict_with_numpy_format['train'].column_names}",
         )
     train_indices, test_indices = splitter.get_split_indexes(column_data_dict)
 
-    train_dataset = dataset.select(train_indices)
-    test_dataset = dataset.select(test_indices)
+    train_dataset = dataset_dict_with_numpy_format["train"].select(train_indices)
+    test_dataset = dataset_dict_with_numpy_format["train"].select(test_indices)
 
     train_test_dataset_dict = datasets.DatasetDict({"train": train_dataset, "test": test_dataset})
     train_test_dataset_dict.save_to_disk(out_path)
